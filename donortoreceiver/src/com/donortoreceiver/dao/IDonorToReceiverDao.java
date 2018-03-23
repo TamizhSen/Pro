8ca834e49e106ddc1d9b8b91229c3bae0bebda96
satishreddynulaka <33915092+satishreddynulaka@users.noreply.github.com>
2018-03-23 02:24:31 -0400

    adding the remaining functionalities

--- donortoreceiver/src/com/donortoreceiver/dao/IDonorToReceiverDao.java
+++ donortoreceiver/src/com/donortoreceiver/dao/IDonorToReceiverDao.java
@@ -5,6 +5,7 @@ package com.donortoreceiver.dao;
 
 import java.util.List;
 
+import com.donortoreceiver.beans.Donor;
 import com.donortoreceiver.beans.ReceiverMessage;
 import com.donortoreceiver.beans.Transactions;
 import com.donortoreceiver.beans.UserDetails;
@@ -23,5 +24,10 @@ public interface IDonorToReceiverDao {
 	public String forgotPassword(String userName);
 	public Boolean postMessage(ReceiverMessage receiverMessage);
 	public boolean updatePassword(String password, String userName);
+	public List<ReceiverMessage> getReceiverMessages(String all);
+	public List<ReceiverMessage> getReceiverMessages(String type,String all);
+	public void insertTransaction(List<Transactions> LTransactions);
+	public void donated(List<Donor> donor);
+	public boolean updateReceiverMessage(List<String> ids);
 	
 }
---
+++ donortoreceiver/src/com/donortoreceiver/extractors/ReceiverMessageExtractor.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package com.donortoreceiver.extractors;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.ResultSetExtractor;
+
+import com.donortoreceiver.beans.ReceiverMessage;
+
+/**
+ * @author Satish Reddy
+ *
+ */
+public class ReceiverMessageExtractor implements ResultSetExtractor<ReceiverMessage> {
+
+	@Override
+	public ReceiverMessage extractData(ResultSet resultSet) throws SQLException, DataAccessException {
+		ReceiverMessage receiverMessage = new ReceiverMessage();
+		/*select idReceiver,category,message,updated,username,name,phone,approved from receiver where approved='YES';*/
+		try {
+			receiverMessage.setId(resultSet.getString(1));
+			receiverMessage.setCategory(resultSet.getString(2));
+			receiverMessage.setMessage(resultSet.getString(3));
+			receiverMessage.setName(resultSet.getString(4));
+			receiverMessage.setPhone(resultSet.getString(5));
+			receiverMessage.setApproved(resultSet.getString(6));
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+			receiverMessage=null;
+		}
+		
+		return receiverMessage;
+	}
+
+}
